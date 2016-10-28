eaca5e2e7c834e8e00e84c4cae65261e84a58a5e
Vaikus <Vaikus@users.noreply.github.com>
2016-10-28 11:15:38 -0600

    Update bubble.c

--- bubble.c
+++ bubble.c
@@ -2,10 +2,10 @@
 // Function for bubble sort
 void Bubble_Sort(int array[], int size) {
     int temp_int;
-	int flag,i,j;
-    for( i = 0; i < size - 1; i++) {	
+	int flag;
+    for( int i = 0; i < size - 1; i++) {	
         flag=0;
-        for( j = 0; j < size - i - 1; j++) {
+        for( int j = 0; j < size - i - 1; j++) {
             if(array[j] > array[j + 1]) {
                 temp_int = array[j];
                 array[j] = array[j + 1];
